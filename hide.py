3b7de884ad57b89ae17fb4a814f1501f2cf01c6a
George <131269015+itstepuser@users.noreply.github.com>
2023-04-19 17:19:05 +0400

    Self hiding feature

---
+++ hide.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+    import os
+    from jaraco import path
+
+    CHECK_STATE: bool = False # If True, the code only tells you if the parent folder is hidden (R)
+    HIDDEN_FILE_LOCATION: str = "C:/Users/student/Documents/GitHub"
+
+    if CHECK_STATE:
+        print(path.is_hidden(HIDDEN_FILE_LOCATION))
+        exit()
+
+    HIDE: bool = True
+
+    if path.is_hidden(HIDDEN_FILE_LOCATION):
+        HIDE = False
+
+    if HIDE:
+        os.system("attrib +h " + HIDDEN_FILE_LOCATION)
+        print("File hidden")
+    else:
+        os.system("attrib -h " + HIDDEN_FILE_LOCATION)
+        print("File shown")
