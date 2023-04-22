72e4f8db32fabba5cc66248d288f98ef25b29b6a
George <131269015+itstepuser@users.noreply.github.com>
2023-04-22 19:04:54 +0400

    asynchronous functions

--- class/cw/index.js
+++ class/cw/index.js
@@ -1,3 +1,17 @@
-$('.circle')[0].style.width = '400px';
-$('.circle')[0].style.height = '400px';
-$('.circle')[0].style.backgroundColor = 'red';
+const myFunc = async function() {
+    setTimeout(() => {
+        console.log(1234);
+
+    }, 100);
+}
+
+myFunc().then(result => {console.log("Success!")}).catch(result => console.log("ERROR"));
+console.log("This");
+
+try {
+    let a = 5;
+    fsdfsd;
+}
+catch (error) {
+    console.log(a);
+}
